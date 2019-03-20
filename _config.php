31ff31a5e24ea9fdba1a2992723cc0c81b9e67a1
Josh Smith <josh.smith@platocreative.co.nz>
2019-03-20 15:31:22 +1300

    Added class compatibility check.

--- _config.php
+++ _config.php
@@ -1,11 +1,15 @@
 <?php
 
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
+
 global $customDateTemplates;
 $customDateTemplates = array(
 /*
  You can modify the date display by assigning new date templates to any of the following
    date scenarios. Use the above date format keys.
-   
+
 'OneDay' 			=> '$StartMonthNameShort. $StartDayNumberShort, $StartYearLong'
 'SameMonthSameYear' => '$StartMonthNameShort. $StartDayNumberShort - $EndDatNumberShort, $EndYearLong'
 'DiffMonthSameYear' => '$StartMonthNameShort. $StartDayNumberShort - $EndMonthNameShort. $EndDayNumberShort, $EndYearLong'
